ad1d86b8116d8f6264dda4f7e0091b82a2cbc6f3
iavicoliNicolas <nicolaseliavicoli@gmail.com>
2026-03-13 14:14:27 -0300

    SongRepository class and methods

--- src/main/java/Classes/SongRepository.java
+++ src/main/java/Classes/SongRepository.java
@@ -1,4 +1,34 @@
 package Classes;
 
-public class SongRepository {
+import Interfaces.Repository;
+
+import java.util.*;
+
+public class SongRepository implements Repository<Song, UUID> {
+
+    private Map<UUID, Song> songs;
+
+    public SongRepository() {
+        this.songs = new HashMap<>();
+    }
+
+    @Override
+    public void save(Song entity) {
+        songs.put(entity.getId(), entity);
+    }
+
+    @Override
+    public Optional<Song> findById(UUID uuid) {
+        return Optional.ofNullable( songs.get(uuid) );
+    }
+
+    @Override
+    public List<Song> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteById(UUID uuid) {
+        songs.remove(uuid);
+    }
 }
--- src/main/java/Interfaces/Repository.java
+++ src/main/java/Interfaces/Repository.java
@@ -1,10 +1,11 @@
 package Interfaces;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface Repository<T, ID>{
     void save(T entity);
-    T findById(ID id);
+    Optional<T> findById(ID id);
     List<T> findAll();
     void deleteById(ID id);
 }
